b83f61b537ace4d361454626d301851c9ae0c7f4
kyoendo <postagie@gmail.com>
2011-12-01 17:43:08 +0900

    test find method

--- anagram_rspec.rb
+++ anagram_rspec.rb
@@ -1,7 +1,24 @@
 require "rspec"
+require "./anagram"
 
 # require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Anagram do
-  
+  it "should build word list from a file" do
+    dic = File.open('./sample_dic')
+    anagram = Anagram.new(dic)
+    anagram.dic.take(5).should == %w(street sweet tester retest word)
+  end
+
+  it "should find anagrams with word 'street'" do
+    dic = File.open('./sample_dic')
+    anagram = Anagram.new(dic)
+    anagram.find('street').should == ['tester', 'retest']
+  end
+
+  it "should find anagrams with word 'power'" do
+    dic = File.open('./sample_dic')
+    anagram = Anagram.new(dic)
+    anagram.find('tower').should == ['rowet', 'wrote']
+  end
 end
