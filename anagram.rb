304cf4c3cc7fd0df316c513ce1c7d54f95368b56
kyoendo <postagie@gmail.com>
2011-12-01 18:52:42 +0900

    add Anagram.build method which save anagram data to a yaml file

--- anagram.rb
+++ anagram.rb
@@ -1,22 +1,41 @@
+# encoding: UTF-8
+# autoload :YAML, "yaml"
+require "yaml"
+
 class Anagram
-  attr_reader :dic
+  attr_reader :words
   def initialize(file)
-    @dic = file.map { |word| word.chomp.downcase }.uniq
-    @anagrams = build_anagram(@dic)
+    @words = Anagram.build_wordlist(file)
+    @anagrams = Anagram.build_anagram(@words)
   end
 
   def find(word)
-    list = @anagrams[signize(word.downcase)]
-    list ? list - [word] : list
+    list = @anagrams[Anagram.signize(word.downcase)]
+    list ? list - [word] : []
   end
 
-  def build_anagram(words)
+  def self.build_wordlist(file)
+    file.map { |word| word.chomp.downcase }.uniq
+  end
+
+  def self.build_anagram(words)
     words.map { |word| [signize(word), word] }
          .inject({}) { |h, (sign,word)| h[sign] ||= []; h[sign] << word; h }
          .select { |sign, words| words.size > 1 }
   end
 
-  def signize(word)
+  def self.signize(word)
     word.chars.sort.join.intern
+  end
+
+  def self.build(file)
+    words = build_wordlist(file)
+    save(build_anagram words)
+  end
+
+  def self.save(anagrams)
+    path = File.expand_path(File.dirname __FILE__) + '/anagram.yml'
+    raise Errno::EEXIST, "Yaml file exist at #{path}" if File.exist?(path)
+    File.open(path, 'w') { |f| YAML.dump anagrams, f }
   end
 end
--- anagram_rspec.rb
+++ anagram_rspec.rb
@@ -4,21 +4,34 @@ require "./anagram"
 # require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Anagram do
+  before do
+    dic = File.open('./sample_dic')
+    @anagram = Anagram.new(dic)
+    dic.close
+  end
+
   it "should build word list from a file" do
-    dic = File.open('./sample_dic')
-    anagram = Anagram.new(dic)
-    anagram.dic.take(5).should == %w(street sweet tester retest word)
+    @anagram.words.take(5).should == %w(street sweet tester retest word)
   end
 
-  it "should find anagrams with word 'street'" do
-    dic = File.open('./sample_dic')
-    anagram = Anagram.new(dic)
-    anagram.find('street').should == ['tester', 'retest']
+  it "should find anagrams with some words" do
+    dic = {'street' => %w(tester retest), 'tower' => %w(rowet wrote)}
+    dic.each { |sign, words| @anagram.find(sign).should == words }
   end
 
-  it "should find anagrams with word 'power'" do
+  it "should not find anagrams with some words" do
+    words = %w(world hello sweet streets)
+    words.each { |word| @anagram.find(word).should == [] }
+  end
+
+  it "should save anagrams to a yaml file" do
     dic = File.open('./sample_dic')
-    anagram = Anagram.new(dic)
-    anagram.find('tower').should == ['rowet', 'wrote']
+    yaml_path = File.expand_path(File.dirname __FILE__) + '/anagram.yml'
+    if File.exist?(yaml_path)
+      ->{ Anagram.build(dic) }.should raise_error(Errno::EEXIST)
+    else
+      file = Anagram.build(dic)
+      file.path.should == yaml_path
+    end
   end
 end
